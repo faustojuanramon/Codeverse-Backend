b5cb7f3ce9490599f21b74ace1730ec42223c7e5
faustojuanramon <faustojro.o@gmail.com>
2023-08-24 21:06:27 -0600

    configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { UsersModule } from './users/users.module';
       envFilePath: '.env',
       isGlobal: true
     }),
-    MongooseModule.forRoot(process.env.MONGO_OP),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     ProjectsModule,
     FilesModule,
     UsersModule,
