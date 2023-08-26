aaad78e51cef6a9167216ffd6ed11f892a5321d2
faustojuanramon <faustojro.o@gmail.com>
2023-08-26 01:26:01 -0600

    .

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    methods: ["GET","POST","PUT","DELETE"],
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
     allowedHeaders: 'Content-Type, Accept',
     exposedHeaders: 'Custom-Header',
+    optionsSuccessStatus: 204,
+    preflightContinue: true,
     credentials: false
   });
   await app.listen(3000);
