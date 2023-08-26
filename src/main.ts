438f4ca52dcfaf5c32529c5736b00a0075f92330
faustojuanramon <faustojro.o@gmail.com>
2023-08-26 01:42:11 -0600

    .

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://codeverse-front.vercel.app',
+    origin: '*',
     methods: ["GET","POST","PUT","DELETE","OPTIONS"],
     allowedHeaders: 'Content-Type, Accept',
     exposedHeaders: 'Custom-Header',
     optionsSuccessStatus: 204,
     preflightContinue: false,
-    credentials: false
+    credentials: true
   });
   await app.listen(3000);
 }
