9448ba200d60de3290a92a9965b1ce586cb70cba
faustojuanramon <faustojro.o@gmail.com>
2023-08-26 09:18:52 -0600

    co r s

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:true});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://codeverse-front.vercel.app/',
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+    allowedHeaders: 'Content-Type, Accept',
+    exposedHeaders: 'Custom-Header',
+    optionsSuccessStatus: 204,
+    preflightContinue: false,
+    credentials: true
+  });
   await app.listen(3000);
 }
 bootstrap();
