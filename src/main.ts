98bc0d974e9e5c9e3cf31bc9cec67e92dba93275
faustojuanramon <faustojro.o@gmail.com>
2023-08-26 09:13:17 -0600

    more cors

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors:true});
-  app.enableCors({
-    origin: 'https://codeverse-front.vercel.app/',
-    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-    allowedHeaders: 'Content-Type, Accept',
-    exposedHeaders: 'Custom-Header',
-    optionsSuccessStatus: 204,
-    preflightContinue: false,
-    credentials: true
-  });
   await app.listen(3000);
 }
 bootstrap();
